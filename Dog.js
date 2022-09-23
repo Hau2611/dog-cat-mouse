770ff3d8454c33b23f888a7281198808210c24c1
Tran Hai Hau Bui <buitranhaihauagvn@gmail.com>
2022-09-23 16:48:01 +0700

    sayHi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,16 @@
-function Dog() {
-	function.stomach = [];
+var chalk = require('chalk');
+
+function Dog(name) {
+	this.name = name;
+	this.stomach = [];
 }
 
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 };
+
+Dog.prototype.sayHi = function() {
+	console.log('Hi! I am a dog. My name is ' + chalk.blue(this.red)); 
+};
+
+module.exports = Dog;
